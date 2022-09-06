514f903249d722ac3a436a3a3bf3fa83478b098e
raana96 <72408337+raana96@users.noreply.github.com>
2022-09-07 01:58:30 +0430

    Add files via upload

---
+++ theme/palette.js
@@ -0,0 +1,58 @@
+const white = '#FFFFFF';
+const black = '#000000';
+
+export default {
+  black,
+  white,
+    "tonalOffset": 0.2,
+    "background": { "paper": "#fff", "default": "#fafafa" },
+    "contrastThreshold": 3,
+    "grey": {
+      "50": "#fafafa",
+      "100": "#f5f5f5",
+      "200": "#eeeeee",
+      "300": "#e0e0e0",
+      "400": "#bdbdbd",
+      "500": "#9e9e9e",
+      "600": "#757575",
+      "700": "#616161",
+      "800": "#424242",
+      "900": "#212121",
+      "A700": "#616161",
+      "A100": "#d5d5d5",
+      "A400": "#303030",
+      "A200": "#aaaaaa"
+    },
+    "text": {
+      "primary": "rgba(0, 0, 0, 0.87)",
+      "secondary": "rgba(0, 0, 0, 0.54)",
+      "disabled": "rgba(0, 0, 0, 0.38)",
+      "hint": "rgba(0, 0, 0, 0.38)"
+    },
+    "divider": "rgba(0, 0, 0, 0.12)",
+    "secondary": {
+      "main": "#1cd9aa",
+      "light": "rgb(135, 243, 190)",
+      "dark": "rgb(73, 168, 121)",
+      "contrastText": "rgba(0, 0, 0, 0.87)"
+    },
+    "common": { "black": "#000", "white": "#fff" },
+    "error": {
+      "light": "#e57373",
+      "main": "#f44336",
+      "dark": "#d32f2f",
+      "contrastText": "#fff"
+    },
+    "type": "light",
+    "action": {
+      "hoverOpacity": 0.08,
+      "hover": "rgba(0, 0, 0, 0.08)",
+      "selected": "rgba(0, 0, 0, 0.14)",
+      "disabledBackground": "rgba(0, 0, 0, 0.12)",
+      "disabled": "rgba(0, 0, 0, 0.26)",
+      "active": "rgba(0, 0, 0, 0.54)"
+    },
+    "primary": {
+      "main": "#7FBCD2",
+    }
+  };
