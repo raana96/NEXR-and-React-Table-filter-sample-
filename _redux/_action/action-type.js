f7c3ba0438cb9dec0b05e1820143f44c34543d15
raana96 <72408337+raana96@users.noreply.github.com>
2022-09-07 01:52:45 +0430

    Create action-type.js

---
+++ _redux/_action/action-type.js
@@ -0,0 +1,17 @@
+export const IS_LOGIN = "IS_LOGIN";
+export const LOADING = "LOADING";
+export const USER_ID = "USER_ID";
+export const USER_NAME = "IS_USER_NAME";
+export const USER_PASSWORD = "IS_USER_PASSWORD";
+export const USER_SUCCESS = "USER_SUCCESS";
+export const USER_ERROR = "USER_ERROR";
+export const ALERT_SUCCESS = "ALERT_SUCCESS";
+export const ALERT_ERROR = "ALERT_ERROR";
+export const ALERT_CLOSE = "ALERT_CLOSE"; 
+export const Alert_WARRING = "Alert_WARRING";
+export const USER_SHOW_MASSAGE = "USER_SHOW_MASSAGE";
+
+//User
+export const ALL_USERS = "ALL_USERS";
+
+
