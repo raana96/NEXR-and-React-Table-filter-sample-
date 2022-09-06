ed6d9d8a306d6bfdcda4c1eb7c02b09cddf05222
raana96 <72408337+raana96@users.noreply.github.com>
2022-09-07 01:56:49 +0430

    Create index.js

---
+++ includes/index.js
@@ -0,0 +1,77 @@
+/**
+ * Material-ui
+ */
+
+export { default as Button } from "@mui/material/Button";
+export { default as Alert } from "@mui/material/Alert";
+export { default as TextField } from "@mui/material/TextField";
+export { default as Fab } from "@mui/material/Fab";
+export { default as classNames } from "classnames";
+export { default as Avatar } from "@mui/material/Avatar";
+export { default as CssBaseline } from "@mui/material/CssBaseline";
+export { default as FormControl } from "@mui/material/FormControl";
+export { default as Input } from "@mui/material/Input";
+export { default as Popper } from "@mui/material/Popper";
+export { default as Fade } from "@mui/material/Fade";
+export { default as InputLabel } from "@mui/material/InputLabel";
+export { default as LockIcon } from "@mui/icons-material/LockOutlined";
+export { default as Paper } from "@mui/material/Paper";
+export { default as Dialog } from "@mui/material/Dialog";
+export { default as Typography } from "@mui/material/Typography";
+export { default as DialogActions } from "@mui/material/DialogActions";
+export { default as DialogContent } from "@mui/material/DialogContent";
+export { default as DialogContentText} from "@mui/material/DialogContentText";
+export { default as DialogTitle } from "@mui/material/DialogTitle";
+export { default as MuiDialogTitle } from "@mui/material/DialogContent";
+export { default as MuiDialogActions } from "@mui/material/DialogActions";
+export { default as Slide } from "@mui/material/Slide";
+export { default as CheckCircleIcon } from "@mui/icons-material/CheckCircle";
+export { default as ErrorIcon } from "@mui/icons-material/Error";
+export { default as WarningIcon } from "@mui/icons-material/Warning";
+export { default as InfoIcon } from "@mui/icons-material/Info";
+export { default as CloseIcon } from "@mui/icons-material/Close";
+
+export { default as IconButton } from "@mui/material/IconButton";
+export { default as Snackbar } from "@mui/material/Snackbar";
+export { default as SnackbarContent } from "@mui/material/Stack";
+export { default as Grid } from "@mui/material/Grid";
+export { default as Select } from "@mui/material/Select";
+export { default as MenuItem } from "@mui/material/MenuItem";
+export { default as Menu } from "@mui/material/Menu";
+export { default as Box } from "@mui/material/Box";
+
+
+//CARD
+export { default as Card } from "@mui/material/Card";
+export { default as Collapse } from "@mui/material/Collapse";
+export { default as CardHeader } from "@mui/material/CardHeader";
+export { default as CardMedia } from "@mui/material/CardMedia";
+export { default as CardContent } from "@mui/material/CardContent";
+export { default as CardActions } from "@mui/material/CardActions";
+
+// 
+
+/**
+ * Dashboard
+ */
+
+export { default as Badge } from "@mui/material/Badge";
+export { default as AppBar } from "@mui/material/AppBar";
+export { default as Toolbar } from "@mui/material/Toolbar";
+export { default as Drawer } from "@mui/material/Drawer";
+export { default as List } from "@mui/material/List";
+export { default as Divider } from "@mui/material/Divider";
+export { default as MenuIcon } from "@mui/icons-material/Menu";
+export { default as NotificationsIcon } from "@mui/icons-material/Notifications";
+export { default as ChevronLeftIcon } from "@mui/icons-material/ChevronLeft";
+
+export { default as ListItem } from "@mui/material/ListItem";
+export { default as ListItemIcon } from "@mui/material/ListItemIcon";
+export { default as ListItemText } from "@mui/material/ListItemText";
+export { default as ListSubheader } from "@mui/material/ListSubheader";
+export { default as DashboardIcon } from "@mui/icons-material/Dashboard";
+export { default as ShoppingCartIcon } from "@mui/icons-material/ShoppingCart";
+export { default as PeopleIcon } from "@mui/icons-material/People";
+export { default as BarChartIcon } from "@mui/icons-material/BarChart";
+export { default as LayersIcon } from "@mui/icons-material/Layers";
+export { default as AssignmentIcon } from "@mui/icons-material/Assignment";
